e23869dcfdf1e48d9adcc20298bd869dc85bcd62
EkZakh <ekzakh14@gmail.com>
2024-01-25 12:30:08 +0300

    database

---
+++ app/src/main/java/com/ekzakh/weatherapp/data/local/db/FavoriteDatabase.kt
@@ -0,0 +1,28 @@
+package com.ekzakh.weatherapp.data.local.db
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.ekzakh.weatherapp.data.local.model.CityDbModel
+
+@Database(entities = [CityDbModel::class], version = 1, exportSchema = false)
+abstract class FavoriteDatabase : RoomDatabase() {
+    companion object {
+        private var INSTANCE: FavoriteDatabase? = null
+        private val LOCK = Any()
+
+        fun getInstance(context: Context): FavoriteDatabase {
+            INSTANCE?.let { return it }
+            synchronized(LOCK) {
+                val database = Room.databaseBuilder(
+                    context,
+                    FavoriteDatabase::class.java,
+                    "FavoriteDatabase",
+                ).build()
+                INSTANCE = database
+                return database
+            }
+        }
+    }
+}
